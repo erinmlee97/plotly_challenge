568d25efe63a9c56245f8dad212af91d02e6cf62
erinmlee97 <erinmlee97@gmailcom>
2020-09-27 17:09:42 -0400

    added init function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -12,18 +12,53 @@ function buildMetadata(sample) {
         Object.entries(sample).forEach(([key, value]) => {
           var row = sample_metadata.append("p");
           row.text(`${key}: ${value}`);
-        }); 
-        }); 
-      // BONUS: Build the Gauge Chart
-      // buildGauge(data.WFREQ);
+        }) 
+    })
   };
 
-//   Build function to get data
-  function buildChart(sample) {
+// Build function to get data
+  function barChart(sample) {
     var url = `/samples/${sample}`
     d3.json(url).then(function(data) {
       var xValues = data.sample_values;
       var yValues = data.otu_ids;
       var labels = data.otu_labels;
+
+        // Bar Chart
+        var trace1 = {
+            x: xValues.slice(0,10).reverse(),
+            y: yValues.slice(0,10).map(object => `OTU ${object}`).reverse(),
+            hovertext: labels.slice(0,10).reverse(),
+            type: "bar",
+            orientation: "h"
+        }
+
+        var data = [trace1];
+
+        var layout = {
+            title: "Top 10 OTUs"
+        };
+
+        plotly.newPlot("bar", data, layout)
+
     });
 };
+
+function init() {
+    // Use d3 to select the drop down menu
+    var selector = d3.select("#selDataset");
+    d3.json("/names").then((sampleNames) => {
+        sampleNames.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample)
+        });
+        // Use 1st sample to build plots
+        const firstSample = sampleNames[0];
+        barChart(firstSample);
+        buildMetadata(firstSample);
+    });
+};
+
+init()
